b8a571c07422a12dfaa17f05dccf6ce1f2adf464
Giovanni Almeida dos Santos <69327898+devGiovanni95@users.noreply.github.com>
2022-11-12 16:56:02 -0300

    Add files via upload
    Finalizando Pesquisa

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
-function preencherTela(data) {
-  //mostra a imagem do pokemon
-  document.getElementById("Pokemon").src = data.sprites.front_default;
- // Mostra o nome
-  document.getElementById("nomePokemon").innerText = data.name;
-}
+let numId = 0
 
+function preencherTela(data, num, namepoke) {
+  //mostra a imagem do pokemon
+  document.getElementById(num).src = data.sprites.front_default
+  // Mostra o nome
+  document.getElementById(namepoke).innerText = data.name
+}
 
 function idPokemon() {
-  let idPoke = document.getElementById("idPok").value;
-  console.log(idPoke);
+  let idPoke = document.getElementById("idPok").value
+  console.log(idPoke)
+  numId++
+  namepoke = numId + "a"
 
-  //Chama api do pokemon
-  fetch("https://pokeapi.co/api/v2/pokemon/" + idPoke)
-    //converte em json
-    .then((response) => response.json())
-    //tratamento 
-    .then((data) => {
-      preencherTela(data);
-    });
+  if (idPoke > 0 && idPoke < 899) {
+    //Chama api do pokemon
+    fetch("https://pokeapi.co/api/v2/pokemon/" + idPoke)
+      //converte em json
+      .then((response) => response.json())
+      //tratamento
+      .then((data) => {
+        preencherTela(data, numId, namepoke)
+      })
+  } else {
+    alert("Ops!!! Pokemon não cadastrado ainda")
+    numId--
+  }
 }
 
-
+function limpar() {
+  window.location.reload()
+}
